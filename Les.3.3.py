c154d1c253f591d288e77e75bb58d444763e3618
Marusya-ryazanova <70341111+Marusya-ryazanova@users.noreply.github.com>
2020-09-09 10:27:27 +0300

    Add files via upload
    дз_3

---
+++ Les.3.3.py
@@ -0,0 +1,6 @@
+Some_list = [10, 20, 30, 40, 50]
+for i in reversed(Some_list):
+    print(i)
+
+
+
---
+++ Les.3.4.py
@@ -0,0 +1,5 @@
+for i in range (100, 202, 6):
+    if i % 5 == 0:
+        print(i)
+    if i > 150:
+        break
